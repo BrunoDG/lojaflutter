18f35d7d05cb6d74a3e18b7d3d242e3c210da6cb
Bruno Dias <brunodg.dev@gmail.com>
2021-07-23 16:36:00 -0300

    First commit.

---
+++ android/app/src/main/kotlin/br/com/beveldrive/lojavirtual/lojavirtual/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.beveldrive.lojavirtual.lojavirtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lojavirtual/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter\'s Clothing',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Color.fromARGB(255, 4, 125, 141)),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
