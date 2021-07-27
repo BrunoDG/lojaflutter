20a4dbe0bf3356fff4dd73fd30b43a6d23e18ff7
Bruno Dias <brunodg.dev@gmail.com>
2021-07-27 20:18:38 -0300

    Product screen created. Going to Login Screen.

---
+++ lib/datas/product_data.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductData {
+  late String id;
+  late String category;
+
+  late String title;
+  late String description;
+
+  late double price;
+
+  late List images;
+  late List sizes;
+
+  ProductData.fromDocument(DocumentSnapshot snapshot) {
+    id = snapshot.id;
+    title = snapshot["title"];
+    description = snapshot["description"];
+    price = snapshot["price"];
+    images = snapshot["images"];
+    sizes = snapshot["sizes"];
+  }
+}
